2847d636a06e21971a27161c7506406426d8fc6f
Jean-Christophe <loiseau.jc@gmail.com>
2024-05-21 08:25:57 +0200

    Run linter on poisson.py

--- poisson2d/poisson.py
+++ poisson2d/poisson.py
@@ -13,14 +13,15 @@ a = 0.01
 epsilon0 = 8.85e-12
 
 # Create arrays to hold potential values
-phi = np.zeros([M+1,M+1],float)
-#phi[0,:] = V
-phiprime = np.zeros([M+1,M+1],float)
+phi = np.zeros([M+1, M+1], float)
+# phi[0,:] = V
+phiprime = np.zeros([M+1, M+1], float)
 
-def ro(x,y):
-    if x>0.6 and x<0.8 and y>0.6 and y<0.8:
+
+def ro(x, y):
+    if x > 0.6 and x < 0.8 and y > 0.6 and y < 0.8:
         return 1
-    elif x>0.2 and x<0.4 and y>0.2 and y<0.4:
+    elif x > 0.2 and x < 0.4 and y > 0.2 and y < 0.4:
         return -1
     else:
         return 0
@@ -29,25 +30,25 @@ def ro(x,y):
 # Main loop
 begin = datetime.datetime.now()
 delta = 1.0
-while delta>target:
+while delta > target:
 
     # Calculate new values of the potential
     a2 = a**2
-    for i in range(1,M):
-        for j in range(1,M):
+    for i in range(1, M):
+        for j in range(1, M):
 
-            phiprime[i,j] = (phi[i+1,j] + phi[i-1,j] \
-                             + phi[i,j+1] + phi[i,j-1])/4 \
-                    + a2/4/epsilon0*ro(i*a,j*a)																												
+            phiprime[i, j] = (phi[i+1, j] + phi[i-1, j]
+                              + phi[i, j+1] + phi[i, j-1])/4 \
+                + a2/4/epsilon0*ro(i*a, j*a)
 
     # Calculate maximum difference from old values
     delta = np.max(abs(phi-phiprime))
 
     # Swap the two arrays around
-    phi,phiprime = phiprime,phi
+    phi, phiprime = phiprime, phi
 end = datetime.datetime.now()
 dif = end-begin
 print(dif.total_seconds())
 # Make a plot
-plt.imshow(phi,origin='lower')
+plt.imshow(phi, origin='lower')
 plt.savefig("purepython.png")
