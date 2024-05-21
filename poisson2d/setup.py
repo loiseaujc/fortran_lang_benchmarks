d30462dac2cd1b61bba6f58241cb327e4c7aeaca
Jean-Christophe <loiseau.jc@gmail.com>
2024-05-21 08:27:18 +0200

    Run linter on setup.py

--- poisson2d/setup.py
+++ poisson2d/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 setup(
-  name = 'yoo',
-  ext_modules = cythonize("poisson.pyx", annotate=True, language_level=3),
-  include_dirs=[numpy.get_include()],
+    name='yoo',
+    ext_modules=cythonize("poisson.pyx", annotate=True, language_level=3),
+    include_dirs=[numpy.get_include()],
 )
